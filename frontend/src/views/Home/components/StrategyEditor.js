5f8648a1ab6736baed51198e501ce26e179d3921
伯颜 <codecomeon@gmail.com>
2020-08-17 17:28:03 +0800

    更新结构

--- frontend/src/views/Home/components/StrategyEditor.js
+++ frontend/src/views/Home/components/StrategyEditor.js
@@ -1,80 +1,68 @@
-import React, { Component } from 'react';
-import * as strategyServices from '@/services/strategy'
+import React, {Component} from 'react';
 import styles from './StrategyEditor.less';
 import SubStrategy from './SubStrategy'
 
 class StrategyEditor extends Component {
 
-  instance = null;
 
   state = {
     clientHeight: document.documentElement.clientHeight,
+    connectorInstance: null,
   }
+
+  sourceClassName = 'source';
+  targetClassName = 'target';
+  sourceSetClassName = 'source-set';
 
   componentDidMount() {
-    // this.initJsPlumbInstance();
+    this.initConnector();
   }
 
-
-
-  initJsPlumbInstance = () => {
-    this.instance = window.jsPlumb.newInstance({
-      connector: ['Flowchart', { midpoint: 0.1, cornerRadius: 4 }],
-      paintStyle: { strokeWidth: 2, stroke: "#ffa500" },
-      endpoint: ["Dot", { radius: 8 }],
-      endpointStyle: { fill: "#ffa500" },
+  initConnector = () => {
+    const connectorInstance = window.jsPlumb.newInstance({
+      connector: ['Flowchart', {midpoint: 0.1, cornerRadius: 4}],
+      paintStyle: {strokeWidth: 2, stroke: "#ffa500"},
+      endpoint: ["Dot", {radius: 8}],
+      endpointStyle: {fill: "#ffa500"},
       container: "editor"
     });
 
-    const { instance } = this;
-
-    const list1El = window.document.querySelector("#list-1");
-    const list2El = window.document.querySelector("#list-2");
-    const list3El = window.document.querySelector("#list-3");
-    const list4El = window.document.querySelector("#list-4");
-
-    // 设置Source
-    const items = list1El.querySelectorAll(".source");
-    for (let i = 0; i < items.length; i += 1) {
-      instance.makeSource(items[i], {
-        allowLoopback: false,
-        anchor: ["Left", "Right"]
-      });
-    }
-    // 设置Target
-    instance.makeTarget(list2El, {
-      allowLoopback: false,
-      anchor: ["Top"]
-    });
-    instance.makeTarget(list3El, {
-      allowLoopback: false,
-      anchor: ["Top"]
-    });
-    instance.makeTarget(list4El, {
-      allowLoopback: false,
-      anchor: ["Top"]
+    // 绑定事件监听
+    connectorInstance.bind("connection", function (c) {
+      console.log('connection', c);
     });
 
-    // 附加list类名，用于list中的节点联动
-    Array.from(window.document.querySelectorAll('.source-set')).forEach(el => {
-      instance.addList(el);
+    connectorInstance.bind("connection", function (c) {
+      console.log('connectionDetached', c);
+    });
+
+    connectorInstance.bind("connectionMoved", function (c) {
+      console.log('connectionMoved', c);
+    });
+
+    this.setState({
+      connectorInstance,
     });
   }
 
   render() {
-    const { clientHeight, } = this.state;
-    const { subStrategyList = [], AllJudgeTypeList = [] } = this.props
+    const {sourceClassName, targetClassName, sourceSetClassName} = this;
+    const {clientHeight, connectorInstance} = this.state;
+    const {subStrategyList = [], AllJudgeTypeList = []} = this.props;
     return (
-      <div className={styles.container} style={{ height: clientHeight - 265 }} id="editor">
+      <div className={styles.container} style={{height: clientHeight - 265}} id="editor">
         {
           subStrategyList.map((subStrategyItem, index) => {
             return (
               <SubStrategy
+                connector={connectorInstance}
+                sourceClassName={sourceClassName}
+                targetClassName={targetClassName}
+                sourceSetClassName={sourceSetClassName}
                 propsId={`list-${index + 1}`}
                 subStrategyItem={subStrategyItem}
                 key={subStrategyItem.id}
                 AllJudgeTypeList={AllJudgeTypeList}
-
               />
             )
           })
