9e10cecf163f68e7ec30046bef81b257af1cd7d4
chenxiaolong <chenxiaolong@inter-credit.net>
2020-09-14 17:51:38 +0800

    init

---
+++ frontend-collection/config/defaultSettings.js
@@ -0,0 +1,15 @@
+const proSettings = {
+  "navTheme": "dark",
+  "primaryColor": "#1890ff",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "fixSiderbar": true,
+  "menu": {
+    "locale": true
+  },
+  "title": "小Go机器人",
+  "pwa": false,
+  "iconfontUrl": ""
+};
+export default proSettings;
